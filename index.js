a8881f0f7cf8a8df79cec457e3926a6059e0a676
Nelson Rosales <nelson.rosales83@gmail.com>
2022-03-08 16:58:23 +0100

    project init

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+var socket = require("socket.io");
+
+// App setup
+var app = express();
+var server = app.listen(4000, function () {
+  console.log("listening on port 4000");
+});
+
+// Static files
+app.use(express.static("public"));
+
+// socket setup
+var io = socket(server);
+
+io.on("connection", function (socket) {
+  console.log("socket connection established", socket.id);
+
+  socket.on("chat", function (data) {
+    io.sockets.emit("chat", data);
+  });
+
+  socket.on("typing", function (data) {
+    socket.broadcast.emit("typing", data);
+  });
+});
---
+++ public/chat.js
@@ -0,0 +1,34 @@
+// Make connection
+var socket = io.connect("http://localhost:4000");
+
+// Query DOM
+const message = document.getElementById("message");
+const handle = document.getElementById("handle");
+const btn = document.getElementById("send");
+const output = document.getElementById("output");
+const feedback = document.getElementById("feedback");
+const form = document.getElementById("chat-form");
+
+// Clear the form fields
+// Emit events
+btn.addEventListener("click", function () {
+  socket.emit("chat", {
+    message: message.value,
+    handle: handle.value,
+  });
+});
+
+message.addEventListener("keypress", function () {
+  socket.emit("typing", handle.value);
+});
+
+// Listen for events
+socket.on("chat", function (data) {
+  feedback.innerHTML = "";
+  output.innerHTML +=
+    "<p><strong>" + data.handle + ": </strong>" + data.message + "</p>";
+});
+
+socket.on("typing", function (data) {
+  feedback.innerHTML = "<p><em> " + data + " is typing a message....</em></p>";
+});
